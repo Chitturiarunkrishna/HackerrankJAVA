6603d5a9d642a226c092b1253c8116104669a949
VigyatBansal <63166899+VigyatBansal@users.noreply.github.com>
2020-10-01 14:11:06 +0530

    Update Solution.java
    Removed irrelevant brackets

--- Introduction/Java If-Else/Solution.java
+++ Introduction/Java If-Else/Solution.java
@@ -17,25 +17,25 @@ public class Solution
         if(n>=1&&n<=100)
         {
             if(n%2!=0)
-            {
-                System.out.println("Weird");
-            }
+                System.out.println("Weird"); 
             else if(n%2==0)
             {
                 if(n>=2&&n<=5)
-                {
-                    System.out.println("Not Weird");
-                }
+                     System.out.println("Not Weird");
                 else if(n>=6&&n<=20)
-                {
-                    System.out.println("Weird");
-                }
+                     System.out.println("Weird");
                 else if(n>20)
-                {
-                    System.out.println("Not Weird");
-                }
+                     System.out.println("Not Weird");
             }
-        }
-        scanner.close();
+        }      
+        scanner.close();      
     }
 }
+                
+                    
+                
+            
+        
+        
+    
+
